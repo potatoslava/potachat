45d97bab46b3d29009575b31ccb3c69c2da28683
potatoslava <va695095@gmail.com>
2026-03-24 16:31:17 +0300

    force client rebuild v2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// build: 2
 export default defineConfig({
   plugins: [react()],
   resolve: {
